3f189cad3f9ca4539348caac0103dfa7df64c5a8
AntimatterNova <dmvsecondary@gmail.com>
2023-05-22 18:38:08 -0500

    finished the README.md generator and input collection

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+var fs = require('fs');
+var inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
-
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What should the title be?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What should the description say?',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'What information should be displayed in the usage section?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What information should be displayed in the installation section?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What people or resources contributed in the creation of this project?',
+            name: 'contributions',
+        },
+        {
+            type: 'list',
+            message: 'What license is the project protected under? (leave blank for no license)',
+            choices: ['MIT', 'GNU', 'Apache'],
+            name: 'license',
+        },
+        
+    ])
+    .then((answers) => {
+        console.log('creating file. . .');
+        const markdown = generateMarkdown(answers);
+        console.log(markdown);
+        fs.writeFile('README.md', markdown, (err) => {
+            if (err) {
+              console.log('UH OH! There was an error creating the file!\n');
+              console.log('Error: ' + err);
+            } else {
+              console.log('The file was created successfully!');
+            };
+        });
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
 // Function call to initialize app
 init();
+
+//README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badges = {
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    GNU: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    Apache: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    None: ''
+  }
+  return badges[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    MIT: '[MIT](https://choosealicense.com/licenses/mit/)',
+    GNU: '[GNUv3](https://choosealicense.com/licenses/gpl-3.0/',
+    Apache: '[Apache2.0](https://choosealicense.com/licenses/apache-2.0/)',
+    None: ''
+  }
+  return licenseLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license){
+    return `This project is licensed under the ${renderLicenseLink(license)} license.`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function genMarkdown(answers) {
+  return `
+# ${answers.title}
+${renderLicenseBadge(answers.license)}
 
+## Table of Contents
+- [Description.](#description)
+- [Usage Tips.](#usage)
+- [Installation](#installation)
+- [Contributing](#contributing)
+- [License](#license)
+
+<a name='description'></a>
+## Product Description
+${answers.description}
+
+<a name='usage'></a>
+## Usage
+${answers.usage}
+
+<a name='installation'></a>
+## Installation
+${answers.installation}
+
+<a name='contributing'></a>
+## Contributing
+${answers.contributions}
+
+<a name='license'></a>
+## License
+${renderLicenseSection(answers.license)}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = genMarkdown;
+
+//README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
